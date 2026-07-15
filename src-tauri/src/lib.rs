23704f5ac4b7dc4d73b1facd277cba56a61115ca
lizheng <lizheng@whai.pku.edu.cn>
2026-07-15 23:11:06 +0800

    chore(release): 0.2.2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -275,6 +275,6 @@ mod tests {
     #[test]
     fn crate_metadata_matches_project() {
         assert_eq!(env!("CARGO_PKG_NAME"), "zterm");
-        assert_eq!(env!("CARGO_PKG_VERSION"), "0.2.1");
+        assert_eq!(env!("CARGO_PKG_VERSION"), "0.2.2");
     }
 }
